220422d1f09c75e24fb5945215b400d8def03c36
leticiavasconcelos1 <leticia.vasconcelos951@gmail.com>
2026-09-18 12:28:39 -0300

    docs: atualiza banco apos conclusao do crud

--- api-livros/database/biblioteca_db.sql
+++ api-livros/database/biblioteca_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/09/2026 às 19:55
+-- Tempo de geração: 18/09/2026 às 17:28
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
